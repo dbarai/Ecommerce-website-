c537fd85af4330463d78aa8af1229321131ca48a
dbarai <dulalbarai01@gmail.com>
2026-01-30 17:32:11 +0530

    Add footer section to ecommerce includes

--- ecommerce/includes/footer.php
+++ ecommerce/includes/footer.php
@@ -1,1 +1,28 @@
-
+    </div>
+    <footer class="bg-dark text-white mt-5 py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h5>OmniMart</h5>
+                    <p>Your one-stop shop for everything.</p>
+                </div>
+                <div class="col-md-4">
+                    <h5>Quick Links</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="/ecommerce/index.php" class="text-white">Home</a></li>
+                        <li><a href="/ecommerce/search.php" class="text-white">Products</a></li>
+                        <li><a href="/ecommerce/contact.php" class="text-white">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h5>Contact Us</h5>
+                    <p>Email: support@omnimart.com</p>
+                    <p>Phone: +1 234 567 890</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/ecommerce/assets/js/script.js"></script>
+</body>
+</html>
